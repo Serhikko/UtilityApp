55661a2c95825899bb235cfa862fa096cd476de7
Serhii <SERHII.PRYNKEVYCH25@bathspa.ac.uk>
2025-12-05 13:24:03 +0000

    Making utility App

---
+++ Finance Tracker.cpp
@@ -0,0 +1,5 @@
+// Unit converter
+// Create an app that converts values between different units such as temperature
+// (Celsius to Fahrenheit), distance (kilometres to miles), or weight (kilograms to
+// pounds). The app should allow users to select a conversion type, enter a value, and
+// receive the converted result.
---
+++ Unit converter.cpp
@@ -0,0 +1,45 @@
+// Finance Tracker
+// Design a tool to help users manage their personal finances by tracking income,
+// expenses, and calculating their current balance. The app should include options to
+// add transactions, view summaries, and possibly categorise spending.
+
+
+//declare variables(expences,currentBalance,deposit,withdraw,summmary)
+//cout "Please enther your income",
+//cout please enther your expences.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void welcomeMessage()
+{
+    cout << "welcome to my proggram \n";
+}
+
+void viewMenu()
+{
+   //menu description 
+   
+}
+
+void userInsertIncome()
+{
+   //ask user to prompt their Income in pounds
+}
+
+void userExpences()
+{
+   //cout if user had any expences so far?
+   //If true +- from balance,else 
+}
+
+int main()
+{
+    welcomeMessage();
+    viewMenu();
+    userInsertIncome();
+    userExpences();
+
+}
