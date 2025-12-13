a490cd02d62b1c7ebf71cf6169f987ef8ac5d0fb
Serhii <SERHII.PRYNKEVYCH25@bathspa.ac.uk>
2025-12-13 22:55:23 +0000

    Adding a welcome and menu
    Added menu(which is not settled up yet) and added user input

--- Unit converter.cpp
+++ Unit converter.cpp
@@ -5,41 +5,83 @@
 
 
 //declare variables(expences,currentBalance,deposit,withdraw,summmary)
-//cout "Please enther your income",
-//cout please enther your expences.
+
 
 #include <iostream>
 #include <string>
 using namespace std;
 
 
+// Global variables
+double currentBalance = 0.0;
+double monthlyIncome = 0.0;
+
+
+//welcome message
 void welcomeMessage()
 {
-    cout << "welcome to my proggram \n";
+    cout << "|---------------------| \n";
+    cout << "|Welcome to my program| \n";
+    cout << "|---------------------| \n";
+    cout << "\nTo begin managing your balance please enter your average monthly income (in pounds):\n£ ";
 }
 
+//display menu funciton
 void viewMenu()
 {
-   //menu description 
-   
+    cout << "\n=== MAIN MENU ===" << endl;
+    cout << "1. Deposit" << endl;
+    cout << "2. Withdraw" << endl;
+    cout << "3. View Balance" << endl;
+    cout << "4. View Transaction Summary" << endl;
+    cout << "5. View Spending by Category" << endl;
+    cout << "6. Exit" << endl;
+    cout << "================" << endl;
+    cout << "Select an option (1-6): ";
 }
+
 
 void userInsertIncome()
 {
-   //ask user to prompt their Income in pounds
+    cin >> monthlyIncome;
+    
+    // gettin input
+    while (monthlyIncome <= 0) 
+    {
+        cout << "Please enter a valid income amount (greater than 0): £ ";
+        cin >> monthlyIncome;
+    }
+    
+    currentBalance = monthlyIncome;
+    cout << "\nYour initial balance has been set to: £" << currentBalance << endl;
 }
 
-void userExpences()
-{
-   //cout if user had any expences so far?
-   //If true +- from balance,else 
-}
+// void userExpences()
+// {
+//    //cout if user had any expences so far?
+// }
+
+// void balanceCalculation()
+// {
+//    //if user had any expences already proceeed to calculation
+// }
+
+// void spendingCategiries()
+// {
+//    //set number of spending categiries to refer to them in future eg.house,food,vehicles and transport,entertainment.
+// }
+
+// void warningMessage()
+// {
+//    //if spending in some category is higher then some % cout you need to cut the budget in some area
+// }
+
 
 int main()
 {
-    welcomeMessage();
-    viewMenu();
-    userInsertIncome();
-    userExpences();
+   welcomeMessage();
+   userInsertIncome();
+    
+ 
 
 }
