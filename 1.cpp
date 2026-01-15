e4af4afe76724175fb4aeafd0d031181bb8672d8
Serhii <SERHII.PRYNKEVYCH25@bathspa.ac.uk>
2026-01-15 17:27:30 +0000

    delete unit converter

--- 1.cpp
+++
@@ -1,5 +0,0 @@
-// Unit converter
-// Create an app that converts values between different units such as temperature
-// (Celsius to Fahrenheit), distance (kilometres to miles), or weight (kilograms to
-// pounds). The app should allow users to select a conversion type, enter a value, and
-// receive the converted result.
